d6b695730ca5084f9ff27e6e88a639f3d5869b9a
Dmitry Kulchitsky <kulchitskydmitry@gmail.com>
2023-03-02 11:28:45 +0400

    First commit

---
+++ testdata/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return render(request, 'about.html')
+
+def generator(request):
+    return HttpResponse('My Generator Script')
